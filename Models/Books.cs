c487ccf4c8c0c85719e36c8ea7d841ade424e051
rafaelcrisan <daniel.rafael.crisan@stud.ubbcluj.ro>
2025-11-12 12:34:33 +0200

    Lab5

--- Models/Books.cs
+++ Models/Books.cs
@@ -26,6 +26,8 @@ namespace Crisan_Daniel_Lab2.Models
         public int? AuthorID { get; set; }
 
         public Author? Author { get; set; }
+
+        public ICollection<Borrowing>? Borrowings { get; set; }
         public ICollection<BookCategory>? BookCategories { get; set; }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Crisan_Daniel_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +9,12 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Crisan_Daniel_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Crisan_Daniel_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Crisan_Daniel_Lab2Context' not found.")));
 
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Crisan_Daniel_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Crisan_Daniel_Lab2Context' not found.")));
+
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LibraryIdentityContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
